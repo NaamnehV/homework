9cf621201685c827c2496823c53e45eb9342d76b
NaamnehV <135263424+NaamnehV@users.noreply.github.com>
2023-06-15 00:27:41 +0300

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,47 @@
+package Homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("\" Работу выполнила Виктория Василенко \"");
+
+        int i = 3333;
+        byte b = -12;
+        long l = 8888L;
+        short s = 8;
+        float f = 23.3F;
+        boolean bl = true;
+        double d = 88.88;
+        char ch = '7';
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(bl);
+        System.out.println(d);
+        System.out.println(ch);
+
+
+        System.out.println("\n");
+
+        i = 6666;
+        b = 12;
+        l = 7777L;
+        s = 4;
+        f = 46.6F;
+        bl = false;
+        d = 44.44;
+        ch = 'h';
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(bl);
+        System.out.println(d);
+        System.out.println(ch);
+
+    }
+}
